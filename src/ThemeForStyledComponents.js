3e861ad0d80f4817ab9dc3fce0f8bdfe5f8baa43
Tigran Galstyan <galstyantigran97@gmail.com>
2017-10-03 01:18:20 +0400

    Theme updated.

--- src/ThemeForStyledComponents.js
+++ src/ThemeForStyledComponents.js
@@ -1,5 +1,13 @@
 const theme = {
-    TextColorMain: 'green',
+    // Text colours
+    TextColorMain: 'rgb(0, 0, 0)',
+
+
+    // Font-Sizes
+    HeaderSize: '20px', // isn't used yet
+
+    // Icon colours
+    SecondaryColor: 'rgb(155, 155, 155)',
 };
 
 export default theme;
