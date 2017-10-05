7d7efc1774b54c01c6f8d0ba2ddbd067fa6ad3ab
Tigran Galstyan <galstyantigran97@gmail.com>
2017-10-06 01:26:02 +0400

    Routing is added.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
-import React, { Component } from 'react';
-import { ThemeProvider } from 'styled-components';
+import React, {Component} from 'react';
+import 'typeface-roboto';
+import {ThemeProvider} from 'styled-components';
+import {
+    BrowserRouter as Router,
+    Route,
+} from 'react-router-dom';
+import AboutUs from './components/AboutUs';
 import theme from './ThemeForStyledComponents';
 import Header from './components/Header';
-// import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-        <ThemeProvider theme={theme}>
-          <Header/>
-        </ThemeProvider>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <ThemeProvider theme={theme}>
+                    <div>
+                        <Router>
+                            <div>
+                                <Header
+                                    firstLoadedRoute={window.location.href.replace(/(.+\w\/)(.+)/,"/$2")}
+                                />
+                                <Route exact path="/" component={() => <h1>Home</h1>}/>
+                                <Route path="/about-us" component={AboutUs}/>
+                            </div>
+                        </Router>
+                    </div>
+                </ThemeProvider>
+            </div>
+        );
+    }
 }
 
 export default App;
