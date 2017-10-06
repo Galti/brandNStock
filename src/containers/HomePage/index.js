6e5d47c2f6ec348d08b9c8065ed9f17271921aaf
Tigran Galstyan <galstyantigran97@gmail.com>
2017-10-06 11:52:29 +0400

    Containers directory is created for PAGES. HomePage is initialized.

---
+++ src/containers/HomePage/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import MainSlider from '../../components/MainSlider';
+
+class HomePage extends React.Component {
+    render() {
+        return(
+            <div>
+                <MainSlider/>
+            </div>
+        )
+    }
+}
+
+export default HomePage;
